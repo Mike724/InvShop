eaf6622e307adff888e31e3fd4299217ff62a307
Mike724 <MichaelMeyer724@gmail.com>
2012-08-09 12:05:20 -0400

    Simple plugin base

---
+++ src/com/mike724/invshop/InvShop.java
@@ -0,0 +1,25 @@
+package com.mike724.invshop;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class InvShop extends JavaPlugin {
+	
+	private Logger log;
+	private PluginDescriptionFile pdf;
+
+	@Override
+	public void onDisable() {
+		log.info(pdf.getName()+" v"+pdf.getVersion()+" has been disabled.");
+	}
+
+	@Override
+	public void onEnable() {
+		log = this.getLogger();
+		pdf = this.getDescription();
+		log.info(pdf.getName()+" v"+pdf.getVersion()+" has been enabled.");
+	}
+
+}
